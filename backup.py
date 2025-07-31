b6396bf1fc1ddba1281f65b4d66dfd9d9a4f3b69
Ronald Lu <rlu22@myseneca.ca>
2025-07-31 17:42:49 -0400

    added skeleton of script

---
+++ backup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import datetime
+import os
+
+'''
+OPS445 Assignment 2 - Group 2
+Program: assignment2.py: backup.py
+Author: Ronald Lu
+Semester: Summer 2025
+Description: This script contains functions to scan specified directories to identify files for full or incremental backups.
+Saves a list of file paths to a manifest file for later compression.
+'''
+
+def excluded_files():
+    pass
+
+def full_backup():
+    pass
+
+def incremental_backup():
+    pass
+
+def file_manifest():
+    pass
