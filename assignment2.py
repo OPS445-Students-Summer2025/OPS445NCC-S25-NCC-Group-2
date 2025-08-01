577a71ca19cbb050a6aec49697c0d10837e9cb09
Prativa Lamichhane <plamichhane2@myseneca.ca>
2025-07-31 23:18:15 -0400

    Initial JSON logging

---
+++ assignment2.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import json
+from datetime import datetime
+
+def log_operation(op_type, source, destination):
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+
+    log_entry = {
+        "time": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        "operation": op_type,
+        "source": source,
+        "destination": destination
+    }
+
+    json_file = "logs/log.json"
+    if os.path.exists(json_file):
+        with open(json_file, "r") as jf:
+            try:
+                data = json.load(jf)
+            except:
+                data = []
+    else:
+        data = []
+
+    data.append(log_entry)
+
+    with open(json_file, "w") as jf:
+        json.dump(data, jf, indent=2)
+
+    print("JSON log saved!")
